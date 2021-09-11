9a5b585cc7b421e0b667c18fddf9a521c189b93d
monikapatidar27 <monikapatidar33440@gmail.com>
2021-09-11 16:17:42 +0530

    first

--- Basic_language_ques/Inverted triangle of star.cpp
+++ Basic_language_ques/Inverted triangle of star.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-  int n;
-  cin>>n;
+  int num;
+  cin>>num;
   
-  for(int i=n;i>=1;i--){
-      for(int j=1;j<=n-i;j++){
+  for(int i=num;i>=1;i--){
+      for(int j=1;j<=num-i;j++){
           cout<<" ";
       }
       for(int j=1;j<=2*i-1;j++){
