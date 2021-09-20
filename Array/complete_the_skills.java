37d0d203d0c8e663a4a938fea2950b70f25465b3
Luv Sahu <lksahuji365@gmail.com>
2021-09-20 22:09:30 +0530

    Added sol for complete_the_skills

--- Array/complete_the_skills.java
+++ Array/complete_the_skills.java
@@ -6,33 +6,34 @@ class complete_the_skills{
     public static void main(String[] args)throws IOException {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        while(t-->0){
-        int n=3;
+        while(t-->0)
+        {
+            int n=3;
 
-        long[] a=new long[n];
-        for(int i=0;i<3;i++)
-            a[i]=sc.nextLong();
+            long[] a=new long[n];
+            for(int i=0;i<3;i++)
+                a[i]=sc.nextLong();
 
-        long[] b=new long[n];
-        for(int i=0;i<3;i++)
-            b[i]=sc.nextLong();
-        
-        solution obj=new solution();
-        obj.scores(a, b);
-        System.out.println(ca+" "+cb);
+            long[] b=new long[n];
+            for(int i=0;i<3;i++)
+                b[i]=sc.nextLong();
+            
+            scores(a, b);
+            System.out.println(ca+" "+cb);
         }
         sc.close();
     }
-    class solution{
-        public void scores(long a[], long b[]) {
-            complete_the_skills.ca=0;
-            complete_the_skills.cb=0;
-            for(int i=0;i<3;i++){
-                if(a[i]>b[i])
-                    complete_the_skills.ca+=1;
-                else if(a[i]<b[i])
-                    complete_the_skills.cb+=1;
-            }
+    static void scores(long a[], long b[]) {
+        ca=0; // On GFG use GFG.ca
+        cb=0; // On GFG use GFG.cb 
+        for(int i=0;i<3;i++){
+            // if a[i] is greater than b[i] then we increament ca
+            if(a[i]>b[i])
+                complete_the_skills.ca+=1;
+            // else if a[i] is less than b[i] then we increament cb
+            else if(a[i]<b[i])
+            // We do not provide else condition because nothing is to be done when the values are equal
+                complete_the_skills.cb+=1;
         }
     }
 }
